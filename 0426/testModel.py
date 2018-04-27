d9bb48c6126ee7db200536f4aecd6cf6192c36ad
shandong115 <534025364@qq.com>
2018-04-27 20:21:06 +0800

    add testModel.py

---
+++ 0426/testModel.py
@@ -0,0 +1,13 @@
+from gensim.models import Word2Vec
+import sys
+
+model = sys.argv[1]
+#print("model:",model)
+word2vec_model = Word2Vec.load(model)
+
+testwords = ['苹果','数学','学术','白痴','篮球','中国','汽车','水杯','china']
+
+for i in range(len(testwords)):
+	res = word2vec_model.most_similar(testwords[i])
+	print("word:%s" % testwords[i])
+	print("res:%s" % res)
---
+++ 0426/word2vec.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+import tensorflow as tf
+import numpy
+import math
+import collections
+import pickle as pkl
+import re
+import jieba
+import os
+import os.path as path
+

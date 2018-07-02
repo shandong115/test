f2ddaa039ba628c628579eb02b6a237bbe4be9f7
shandong115 <534025364@qq.com>
2018-07-02 15:33:08 +0800

    modify some files

--- dealfile.sh
+++ dealfile.sh
@@ -1,38 +1,52 @@
 #!/bin/sh
-dir=$1
-echo $1
-start=$2
-end=$3
-out=$4
+echo "flag is $1"
+flag=$1
+dir=$2
+start=$3
+end=$4
+out=$5
+echo $start
+echo $end
 
 function cvtSeconds(){
-	echo time is $1
+#	echo time is $1
 	t=$1
-	h=${t:0:2}
-	m=${t:3:2}
-	s=${t:6:2}
-	ms=${t:9}
-	let tt=`expr $h*60*60+$m*60+$s+$ms/100`
-	return $tt
+	let h=${t:0:2}
+	let m=${t:3:2}
+	let s=${t:6:2}
+#	ms=${t:9}
+#	echo "hour is $h"
+#	echo "minutes is $m"
+#	echo "seconds is $s" 
+	let tt=${h}*60*60+${m}*60+${s}
+	echo $tt
+#	return $tt
 }
 
-if test -d $1
-then
-	cd $1
+if [ $flag -eq 1 ]; then
+	cd $dir
 	flist=`ls`
+	i=1
 	for ff in $flist
 	do
 		if test -f $ff
 		then
 			time=`ffmpeg -i $ff 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//`
 			echo $ff total time is $time
-			cvtSeconds $time
-			ss=$?
+	#		cvtSeconds $time
+	#		ss=$?
+			ss=`cvtSeconds $time`
 			echo $ss
 			let dd=$ss-$start-$end
 			echo $dd
-			a=`ffmpeg -ss $start -t $dd  -accurate_seek -i $ff -codec copy -avoid_negative_ts 1 $out/$ff -y`
+			if [ $dd -lt 0 ]; then
+				start=0
+				dd=$time
+			fi
+
+			a=`ffmpeg -ss $start -t $dd -accurate_seek -i $ff -codec copy -avoid_negative_ts 1 $out/${i}_${ff} -y`
 		#	mv cut.mp4 $out/$ff
+			let i+=1
 		fi
 		if test -d $ff
 		then
@@ -41,21 +55,21 @@ then
 	done
 	cd -
 fi
-#for file in $dir/*
-#filelist=`ls $1/*`
-#for file in $filelist
-#do
-#	if test -f $file
-#	then
-#		echo $file is file
-#		time=`ffmpeg -i $file 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//`
-#		echo $file total time is $time
-#		cvtSeconds $time
-#		ss=$?
-#		echo $ss
-#	fi
-#	if test -d $file
-#	then
-#		echo $file is dir
-#	fi
-#done 
+
+if [ $flag -eq 0 ]; then
+	cd $dir
+	ff=$6
+	time=`ffmpeg -i $ff 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//`
+	echo $ff total time is $time
+	ss=`cvtSeconds $time`
+	echo $ss
+	let dd=$ss-$start-$end
+	echo $dd
+	if [ $dd -lt 0 ]; then
+		start=0
+		dd=$time
+	fi
+
+	a=`ffmpeg -ss $start -t $dd -accurate_seek -i $ff -codec copy -avoid_negative_ts 1 $out/${ff} -y`
+	cd -
+fi
--- deleteflv.py
+++ deleteflv.py
@@ -1,19 +1,31 @@
 import os
-s1='.flv'
+import sys
+#s1='.flv'
+s1='-'
 s2='.mp4'
 filename=''
 fileNum=0
 changeNum=0
+dir=sys.argv[1]
+print("dir is %s"%dir)
+os.chdir(dir)
 for ss in os.listdir("."):
 	print ss
-	n1=ss.index(s1)
-	n2=ss.index(s2)
-	fileNum=fileNum+1
-	if(n1>0 and n2>0):
-		filename=ss[0:n1]
-		filename+=ss[n2:]
-		print filename
-		os.rename(ss, filename)
-		changeNum=changeNum+1
+	try:
+		n1=ss.index(s1)
+		n2=ss.index(s2)
+		fileNum=fileNum+1
+		if(n1>0 and n2>0):
+			filename=ss[0:n1]
+			filename+=ss[n2:]
+			print filename
+			os.rename(ss, filename)
+			changeNum=changeNum+1
+	except ValueError:
+		print "ValueError"
+	except WindowsError:
+		print "windows error"
+	else:
+		print "rename ok!"
 print("fileNum:"+str(fileNum))
 print("changeNum:"+str(changeNum))
